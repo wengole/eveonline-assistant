2c8b76030a241703c76e34244e0ae59fb79c98cb
Ben Cole <benc@netsight.co.uk>
2014-03-28 15:29:24 +0000

    Changed - Language

--- django_eve_mon/config/settings.py
+++ django_eve_mon/config/settings.py
@@ -136,10 +136,10 @@ class Common(Configuration):
 
     ########## GENERAL CONFIGURATION
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
-    TIME_ZONE = 'America/Los_Angeles'
+    TIME_ZONE = 'Europe/London'
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-    LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-gb'
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
     SITE_ID = 1
