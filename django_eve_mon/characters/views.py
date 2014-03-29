36c9cf648f7f3566f48b82fbfa9d9cac05103bad
Ben Cole <wengole@gmail.com>
2014-03-29 20:33:58 +0000

    Added - add character view now actually gets characters from EVE API

--- django_eve_mon/characters/views.py
+++ django_eve_mon/characters/views.py
@@ -2,6 +2,9 @@ from time import sleep
 from braces.views import JSONResponseMixin
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from evelink.account import Account
+from evelink.api import API
+from evelink.eve import EVE
 
 
 class AddCharacter(JSONResponseMixin, TemplateView):
@@ -10,10 +13,22 @@ class AddCharacter(JSONResponseMixin, TemplateView):
 
     def post(self, request):
         self.content_type = "application/json"
+
+        key_id = int(request.POST.get('keyid'))
+        vcode = request.POST.get('vcode')
+        characters = self.get_characters_from_api(key_id, vcode)
         context = {
-            'data': [
-                {'id': 123,
-                 'text': 'Cpt. Failsauce'},
-            ],
+            'data': characters,
         }
         return self.render_json_response(context)
+
+    def get_characters_from_api(self, key_id, vcode):
+        api = API(api_key=(key_id, vcode))
+        account = Account(api=api)
+        characters = account.characters()[0]
+        return [
+            {
+                'id': x,
+                'text': characters[x]['name']
+            } for x in characters.keys()
+        ]
