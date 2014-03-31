578bf50674a5a2e3ae1d08d369fa315212991151
Ben Cole <wengole@gmail.com>
2014-03-31 21:19:33 +0100

    Added - Staff members have access to all :D

--- django_eve_mon/characters/utils.py
+++ django_eve_mon/characters/utils.py
@@ -4,4 +4,4 @@ from braces.views import UserPassesTestMixin
 class UserIsOwnerMixin(UserPassesTestMixin):
 
     def test_func(self, user):
-        return self.get_object().is_owner(user)
+        return self.get_object().is_owner(user) or self.request.user.is_staff
