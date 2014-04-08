7219126e7fa26e52c48c4dc9856a0dc592204cd2
Ben Cole <benc@netsight.co.uk>
2014-04-08 12:56:29 +0100

    Added - Docstrings

--- django_eve_mon/skills/views.py
+++ django_eve_mon/skills/views.py
@@ -1,9 +1,15 @@
+"""
+Views for the Skills app
+"""
 from django.views.generic import ListView, FormView
 from .models import Group, Skill
 from .forms import AddSkillToPlanForm
 
 
 class SkillsInGroups(ListView):
+    """
+    Browse all skills view
+    """
     model = Group
 
 
