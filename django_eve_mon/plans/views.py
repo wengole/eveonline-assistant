1d1e600077d902583559eb48ac75499b8325056e
Ben Cole <benc@netsight.co.uk>
2014-04-08 12:22:51 +0100

    Added - Docstrings

--- django_eve_mon/plans/views.py
+++ django_eve_mon/plans/views.py
@@ -1,3 +1,6 @@
+"""
+Views for the Plans app
+"""
 from braces.views import LoginRequiredMixin
 from django.views.generic import CreateView, UpdateView, DetailView
 from .models import Plan
@@ -6,22 +9,39 @@ from skills.models import Group
 
 
 class AddPlan(LoginRequiredMixin, CreateView):
+    """
+    Add a plan
+    """
     model = Plan
     form_class = PlanForm
 
     def form_valid(self, form):
+        """
+        AdSet user of Plan instance to current user
+        :param form: Passed in by Django
+        """
         form.instance.user = self.request.user
         return super(AddPlan, self).form_valid(form)
 
 
 class ManagePlans(LoginRequiredMixin, UpdateView):
+    """
+    List plans
+    """
     model = Plan
 
 
 class PlanDetail(LoginRequiredMixin, DetailView):
+    """
+    Edit a plan
+    """
     model = Plan
 
     def get_context_data(self, **kwargs):
+        """
+        Add groups (and therefore skills) to plan detail for skill browser
+        :param kwargs: Passed in by Django
+        """
         context = super(PlanDetail, self).get_context_data(**kwargs)
         context.update({
             'group_list': Group.objects.all()
