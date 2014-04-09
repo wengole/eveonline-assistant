7e02b8df5d981b2d75f46c4d90905ca6074456ce
Ben Cole <benc@netsight.co.uk>
2014-04-09 22:07:41 +0100

    Added - GetOrNoneManager as it's well handy

--- django_eve_mon/core/utils.py
+++ django_eve_mon/core/utils.py
@@ -1,4 +1,5 @@
 from django.core.cache import cache
+from django.db import models
 from evelink import api
 import time
 
@@ -12,3 +13,17 @@ class DjangoCache(api.APICache):
     def put(self, key, value, duration):
         expiration = time.time() + duration
         self.cache.set(key, (value, expiration), duration)
+
+
+class GetOrNoneManager(models.Manager):
+    """Adds get_or_none method to objects
+    """
+
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            # TODO: Sort out logging
+            #logger.warn('No %s matching kwargs %s' % (self.model._meta
+            # .verbose_name, kwargs))
+            return None
