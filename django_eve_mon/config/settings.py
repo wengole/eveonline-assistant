d3d9ac7e40b246ee1345456ca888aa26e16f8af6
Ben Cole <wengole@gmail.com>
2014-04-12 10:43:05 +0100

    Added - Test configuration

--- django_eve_mon/config/settings.py
+++ django_eve_mon/config/settings.py
@@ -473,3 +473,34 @@ class Heroku(Common):
         ########## END CACHING
 
         ########## Your production stuff: Below this line define 3rd party libary settings
+
+
+class Test(Common):
+    ########## INSTALLED_APPS
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    ########## END INSTALLED_APPS
+
+    ########## Mail settings
+    EMAIL_HOST = "localhost"
+    EMAIL_PORT = 1025
+    EMAIL_BACKEND = values.Value(
+        'django.core.mail.backends.console.EmailBackend')
+    ########## End mail settings
+
+    ########## django-debug-toolbar
+    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INSTALLED_APPS += ('debug_toolbar',)
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TEMPLATE_CONTEXT': True,
+    }
+    ########## end django-debug-toolbar
+
+    ########## Your local stuff: Below this line define 3rd party libary settings
+    DATABASES = values.DatabaseURLValue(
+        'postgres://localhost/test_django_eve_mon'
+    )
