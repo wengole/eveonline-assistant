545ed3899b90a76483dfb7747160df5945e505ab
Ben Cole <benc@netsight.co.uk>
2014-04-12 10:28:19 +0100

    WiP - Plans test

--- django_eve_mon/plans/tests.py
+++ django_eve_mon/plans/tests.py
@@ -1,10 +1,22 @@
 from django.test import TestCase
-from skills.models import Skill
+from characters.models import Character, ApiKey
+from users.models import User
 
 
 class TestPlan(TestCase):
+    fixtures = ['skills']
+
     @classmethod
     def setUpClass(cls):
-        skill_1 = Skill(
+        cls.user = User.objects.create(
+            
+        )
+        cls.apikey = ApiKey.objects.create(
+            key_id = '1234',
+            verification_code='abc123',
+
+        )
+        cls.character = Character.objects.create(
+            apikey=apikey,
 
         )
