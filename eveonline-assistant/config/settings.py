c6646ceddfa0dac24c165777bd85e9cff5975ba5
Ben Cole <benc@netsight.co.uk>
2014-04-14 10:49:34 +0100

    Fixed - from email domain doesn't exist

--- eveonline-assistant/config/settings.py
+++ eveonline-assistant/config/settings.py
@@ -337,7 +337,7 @@ class VPS(Common):
 
     ########## EMAIL
     DEFAULT_FROM_EMAIL = values.Value(
-        'EVE Online Assistant <noreply@django-eve-mon.com>'
+        'EVE Online Assistant <noreply@wengole.com>'
     )
     EMAIL_HOST = values.Value('localhost')
     EMAIL_PORT = values.IntegerValue(25, environ_name='EMAIL_PORT')
